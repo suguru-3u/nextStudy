cb20de24d9f3656a0cb77ca391b520fb256ec25d
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-24 10:12:36 +0900

    update  index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
+const h1 ={
+    fontSize:"72pt",
+    fontWeight:"bold",
+    textAlign:"right",
+    leterSpacing:"-8px",
+    color:"f0f0f0",
+    margin:"-40px 0px"
+}
+
+const p ={
+    fontSize:"16pt",
+    color:"#666",
+    margin:"0px"
+}
+
+
+
 export default () =><div>
-    <h1>Next.js</h1>
-    <div>Wellcome</div>
+    <h1 style={h1}>Next.js</h1>
+    <p style={p}>Wellcome</p>
 </div>
+
