b30d5f0b58b51a3078926d64eeafe0f78f8a50f9
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-24 10:53:57 +0900

    create  Style.js

---
+++ static/Style.js
@@ -0,0 +1,17 @@
+export default () =><div>
+    <style jsx>{`
+        h1{
+            font-size:72pt;
+            font-weight:bold;
+            text-align:right;
+            leter-spacing:-8px;
+            color:f0f0f0;
+            margin:-30px 0px;
+        }
+        p{
+             font-size:16p
+             t;
+             color:#666;
+             margin:0px;
+        }
+    `}</style>
