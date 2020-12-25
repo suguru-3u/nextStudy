91c6542f9cdeb3c8049412871d041503b14a1630
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-25 09:53:01 +0900

    study next Redux

--- store.js
+++ store.js
@@ -1,1 +1,37 @@
-    
+import {createStore,applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+// ステート初期化
+const initial = {
+    message:'START',
+    count:0
+}
+
+// レデユーサー
+function counterReducer(state = initial,action){
+    switch(action.type){
+        case'INCREMENT':
+            return{
+                message:'INCREMENT',
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return{
+                message:'DECREMENT',
+                count:state.count - 1
+            };
+        case 'RESET':
+            return{
+                message:'RESET',
+                count:initial.count
+            };
+        default:
+            return state;
+    }
+}
+
+// iniStore関数
+export function iniStore(state = initial){
+    return createStore(counterReducer,state,
+        applyMiddleware(thunkMiddleware))
+}
