9583511e26c49f68797da70d2c09a65ad7b41e2e
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-29 13:21:18 +0900

    update finish store.js

--- store.js
+++ store.js
@@ -2,39 +2,29 @@ import {createStore,applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import firebase from 'firebase';
 
-// ステート初期化
-const initial = {
-    message:'START',
-    data:[],
-    number:[],
-    result:0
+// Firebaseの初期化
+let donfig = {
+   
 }
 
-// レデユーサー
-function calcReducer(state = initial,action){
+let fireapp;
+try {
+    firebase.initializeApp(config);
+}catch(error){
+    console.log(error.message)
+}
+export default firebase;
+
+// ステート初期化
+const initial = {
+}
+
+// レデユーサー（ダミー）
+function fireReducer(state = initial,action){
     switch(action.type){
           // 計算実行
-        case'ENTER':
-            let data2 = state.data.slice();
-            let s = action.value;
-            data2.unshift(s);
-            let num = s.replace(/[^0-9]/g,"");
-            let number2 = state.number.slice();
-            number2.unshift(num);
-            let result = (state.result * 1) + (num * 1);
-            return{
-                message:'ENTER',
-                data:data2,
-                number:number2,
-                result:result
-            };
-        case 'RESET':
-            return{
-                message:'RESET',
-                data:[],
-                number:[],
-                result:0
-            };
+        case'TESTACTION':
+            return state;
         default:
             return state;
     }
@@ -42,6 +32,6 @@ function calcReducer(state = initial,action){
 
 // iniStore関数
 export function initStore(state = initial){
-    return createStore(calcReducer,state,
+    return createStore(fireReducer,state,
         applyMiddleware(thunkMiddleware))
 }
