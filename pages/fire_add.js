f0473ada55ab1c0f82273d84415a25429493c790
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-29 15:11:04 +0900

    update fire_add.js

---
+++ pages/fire_add.js
@@ -0,0 +1,10 @@
+// import Link from 'next/link';
+import Layout from '../components/Layout';
+import Fireadd from '../components/Fireadd';
+
+export default () =>(
+   <Layout header="Fire" title="add data">
+        <Fireadd/>
+    </Layout >
+);
+
