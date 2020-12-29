7cbbb25bf99b213343b01a4153d524609741e121
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-29 14:27:35 +0900

    Update fire_find.js

---
+++ pages/fire_find.js
@@ -0,0 +1,10 @@
+// import Link from 'next/link';
+import Layout from '../components/Layout';
+import Firefind from '../components/Firefind';
+
+export default () =>(
+   <Layout header="Fire" title="Sample data">
+        <Firefind/>
+    </Layout >
+);
+
